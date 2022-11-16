12a7d54c0172c34439670e5cc4893787a98b2b7a
Hans TW <hans.walheim@gmail.com>
2022-11-16 07:29:47 +0100

    A basic start

---
+++ scripts/t7.py
@@ -0,0 +1,270 @@
+# Download libclang from from
+# https://github.com/llvm/llvm-project/releases/tag/llvmorg-11.0.1,
+# e.g.
+# https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/LLVM-11.0.1-win64.exe
+# https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.1/LLVM-14.0.1-win64.exe
+# Run and install at e.g. D:\LLVM\11.0.1
+#
+# For more
+# https://pypi.org/project/libclang/
+# https://sudonull.com/post/907-An-example-of-parsing-C-code-using-libclang-in-Python
+
+
+import argparse
+import my_python_bindings.cindex as clang
+import json
+import os
+from pprint import pprint
+import sys
+import textwrap
+
+MY_NAME = os.path.basename(__file__)
+
+DESCRIPTION = f"""
+Make a source index of a c or c++ file
+"""
+USAGE_EXAMPLE = f"""
+Example:
+> {MY_NAME} -D YES=1 -I my_include_dir a.c
+"""
+#-------------------------------------------------------------------------------
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        MY_NAME,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent(DESCRIPTION),
+        epilog=textwrap.dedent(USAGE_EXAMPLE)
+    )
+    add = parser.add_argument
+    add('-D', '--define', type=str, action='append',
+        help='Define a macro')
+    add('-I', '--include', type=str, action='append',
+        help='Add include directory')
+    add('-std', '--std', type=str,
+        help='Language standard - to see them type\nclang(++) -std=blaj a.c')
+
+    add('--source_file', required=True, help='Input file')
+
+    options = parser.parse_args()
+    if options.define is None:
+        options.define = []
+    if options.include is None:
+        options.include = []
+
+    return options
+
+#-------------------------------------------------------------------------------
+def load_json_data(file):
+    data = {}
+    with open(file) as fp:
+        try:
+            data = json.load(fp)
+        except json.decoder.JSONDecodeError:
+            print('loading json went wrong')
+
+    return data
+
+#-------------------------------------------------------------------------------
+def store_json_data(file, data):
+    try:
+        with open(file, 'w') as fp:
+            json.dump(data, fp, indent=2)
+    except json.decoder.JSONDecodeError:
+        print('saving json went wrong')
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+def run(tu, the_tree):
+    recurse(tu.cursor, the_tree)
+
+#-------------------------------------------------------------------------------
+def print_location(loc, iter):
+    filename = None
+    if loc.file:
+        filename = loc.file.name
+    indent = ' ' * iter
+    print(f'{indent}{filename}, line: {loc.line}, {loc.column}')
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+def get_location(loc):
+    location = {}
+    filename = None
+    if loc.file:
+        filename = loc.file.name
+    location['filename'] = filename
+    location['line'] = loc.line
+    location['column']= loc.column
+    return location
+
+#-------------------------------------------------------------------------------
+def cursorIsReference(kind):
+    if (kind >= clang.CursorKind.FIRST_REF and
+        kind <= clang.CursorKind.LAST_REF):
+        return True
+    if (kind >= clang.CursorKind.DECL_REF_EXPR and
+        kind <= clang.CursorKind.CALL_EXPR):
+        return True
+    if kind == clang.CursorKind.MACRO_INSTANTIATION:
+        return True
+
+    return False
+
+#-------------------------------------------------------------------------------
+def cursorIsDeclaration(kind):
+    if (kind >= clang.CursorKind.STRUCT_DECL and
+        kind <= clang.CursorKind.OBJC_DYNAMIC_DECL):
+        return True
+    if (kind == clang.CursorKind.LABEL_STMT):
+        return True
+    return False
+
+#-------------------------------------------------------------------------------
+def wanted_cursor(cursor):
+    the_kind = cursor.kind
+    if cursorIsDeclaration(the_kind):
+        return True
+    if cursorIsReference(the_kind):
+        return True
+    if the_kind.is_preprocessing():
+        return True
+    return False
+
+#-------------------------------------------------------------------------------
+def get_cursorUSR(cursor):
+    usr = cursor.get_usr()
+    if usr is not None and len(usr):
+        return usr
+
+    # See if the cursors' reference can give something
+    try:
+        if ref := clang.conf.lib.clang_getCursorReferenced(cursor):
+            if usr := ref.get_usr():
+                if len(usr):
+                    return usr
+    except TypeError:
+        print(f' - not referenceable {cursor.kind}')
+
+    # See if we can get to its definition cursor
+    if definition_cursor := cursor.get_definition():
+        if usr := definition_cursor.get_usr():
+            if len(usr):
+                return usr
+
+    return None
+
+#-------------------------------------------------------------------------------
+def handle_cursor(cursor, the_tree):
+    # Get the USR and use as key
+    usr = get_cursorUSR(cursor)
+    if usr is None:
+        return
+
+    # Prepare contents
+    content = {}
+    content['reference'] = []
+    content['declaration'] = []
+    content['definition'] = []
+
+    # Current source position
+    src_pos = {}
+    src_pos['location'] = get_location(cursor.location)
+    src_pos['cursor']  = f'{cursor.kind}'
+    src_pos['storage_class'] = str(cursor.storage_class)
+#    src_pos['linkage_kind'] = str(cursor.linkage)
+
+    # A definition is also a declaration - so need to nag
+    if cursor.is_definition():
+        ref_type = 'Definition'
+        content['definition'].append(src_pos)
+    elif cursorIsDeclaration(cursor.kind):
+        ref_type = 'Declaration'
+        content['declaration'].append(src_pos)
+    if cursorIsReference(cursor.kind):
+        ref_type = 'Reference'
+        content['reference'].append(src_pos)
+
+    displayname = cursor.displayname
+    if displayname is None or len(displayname) == 0:
+        displayname = '-No name-'
+
+    if usr not in the_tree:
+        content['displayname'] = displayname
+        the_tree[usr] = content
+        return
+
+    curr_content = the_tree[usr]
+    if ref_type == 'Defintion':
+        if src_pos not in curr_content['definition']:
+            curr_content['definition'].append(src_pos)
+    if ref_type == 'Declaration':
+        if src_pos not in curr_content['declaration']:
+            curr_content['declaration'].append(src_pos)
+    if ref_type == 'Reference':
+        if src_pos not in curr_content['reference']:
+            curr_content['reference'].append(src_pos)
+    if 'displayname' in curr_content:
+        if displayname != curr_content['displayname']:
+#            print (f'Funny spelling: {displayname} - {curr_content["displayname"]}')
+            if len(displayname) > len(curr_content['displayname']):
+                curr_content['displayname'] = displayname
+    else:
+        curr_content['displayname'] = displayname
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+def recurse(cursor, the_tree):
+    if wanted_cursor(cursor):
+#        print(f'Cursor: {cursor.kind}')
+#        print_location(cursor.location, 2)
+        handle_cursor(cursor, the_tree)
+    else:
+#        print(f'Skip:   {cursor.kind}')
+#        print_location(cursor.location, 2)
+        pass
+
+    for child in cursor.get_children():
+        recurse(child, the_tree)
+
+#-------------------------------------------------------------------------------
+def make_command_line(inputs):
+    print(20*'=')
+    arguments = ""
+    for the_define in inputs.define:
+        arguments += f'-D {the_define} '
+    for include_dir in inputs.include:
+        arguments += f'-I \"{include_dir}\" '
+
+    return inputs.source_file, arguments
+
+#-------------------------------------------------------------------------------
+def main():
+    inputs = parse_arguments()
+    src_file, arguments = make_command_line(inputs)
+
+    print(f'{src_file = }\n{arguments = }')
+    if not os.path.exists(src_file):
+        print(f'Cannot open file "{src_file}"')
+        sys.exit(3)
+
+    clang.Config.set_library_path('C:/LLVM/14.0.1/bin')
+
+    index = clang.Index.create()
+    translation_unit = index.parse(src_file, args=[arguments])
+    pprint(translation_unit.cursor)
+    the_tree = {}
+    run(translation_unit, the_tree)
+
+    save_file_name = os.path.basename(src_file)
+    save_file_name += '.indx'
+    store_json_data(save_file_name, the_tree)
+    print(f'Output saved as {save_file_name}')
+    return 0
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+sys.exit(main())
---
+++ test/test_t7.bat
@@ -0,0 +1,18 @@
+setlocal
+set INC0="D:/wrk/clang-test/test/ICU/icu4c/include"
+set INC1="C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt"
+set INC2="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.31.31103/include"
+
+set DEF0="_MBCS"
+set DEF1="WIN32"
+set DEF2="_DEBUG"
+set DEF3="WINVER=0x0601"
+set DEF4="_WIN32_WINNT=0x0601"
+set DEF5="_CRT_SECURE_NO_DEPRECATE"
+
+set SRC="D:/wrk/clang-test/test/ICU/icu4c/source/samples/cal/cal.c"
+
+::t7.py -I %INC0% -I %INC1% -I %INC2% -D%DEF0% -D%DEF1% -D%DEF2% -D%DEF3% -D%DEF4% -D%DEF5% %SRC%
+::t7.py -I %INC0% -I %INC1% -I %INC2% -D %DEF4% --source_file %SRC%
+
+..\scripts\t7.py --include %INC0% --include %INC1% --include %INC2% -D%DEF0% -D%DEF1% -D%DEF2% -D%DEF3% -D%DEF4% -D%DEF5% --source_file %SRC%
