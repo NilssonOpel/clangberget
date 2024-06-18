838050e8c1f608a6ae81987d76448214a0686b53
Hans Walheim <hans.walheim@iar.com>
2024-06-18 16:41:21 +0200

    Add some other test-files

--- test/test_t7.bat
+++ test/test_t7.bat
@@ -1,5 +1,5 @@
 setlocal
-set INC0="D:/wrk/clang-test/test/ICU/icu4c/include"
+set INC0="../ICU/icu4c/include"
 set INC1="C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt"
 set INC2="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.31.31103/include"
 
@@ -10,7 +10,8 @@ set DEF3="WINVER=0x0601"
 set DEF4="_WIN32_WINNT=0x0601"
 set DEF5="_CRT_SECURE_NO_DEPRECATE"
 
-set SRC="D:/wrk/clang-test/test/ICU/icu4c/source/samples/cal/cal.c"
+set SRC="../icu/tools/unicode/c/genprops/misc/ucdstrip.c"
+set SRC="../icu/icu4c/source/tools/tzcode/zdump.c"
 
 ::t7.py -I %INC0% -I %INC1% -I %INC2% -D%DEF0% -D%DEF1% -D%DEF2% -D%DEF3% -D%DEF4% -D%DEF5% %SRC%
 ::t7.py -I %INC0% -I %INC1% -I %INC2% -D %DEF4% --source_file %SRC%
